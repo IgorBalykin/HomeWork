d0720b9a2f7e37a3716562fd8951a567288e77f9
IgorBalykin <nuclearcrafter@mail.ru>
2017-07-06 12:08:29 +0300

    Initial commit for DEV-1 code

---
+++ DEV-1/CustomSequenceOutputter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace DEV_1
+{
+    class CustomSequenceOutputter
+    {
+        private const string ERROR_MAIN_MESSAGE = "Looks like something really bad happened, halting with error code";
+        static int Main(string[] args)
+        {
+            try
+            {
+                Sequence seq = new Sequence();
+                seq.print();
+                Console.ReadKey();
+            }
+            catch(Exception)
+            {
+                Console.WriteLine(ERROR_MAIN_MESSAGE);
+                return -1;
+            }
+            return 0;
+        }
+    }
+}
