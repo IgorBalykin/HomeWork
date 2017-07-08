97e75a2ad2eb6c8b68b96a3fa05fcd39d9f994ff
IgorBalykin <nuclearcrafter@mail.ru>
2017-07-08 23:04:23 +0300

    Added main exception handling

--- DEV-7/EntryPoint.cs
+++ DEV-7/EntryPoint.cs
@@ -4,14 +4,30 @@ namespace DEV_7
 {
     class EntryPoint
     {
-        static void Main(string[] args)
+        enum MainStatusCodes
         {
-            ConsoleReader mainReader = new ConsoleReader();
-            TriangleTypeDeterminator mainDeterminator = new TriangleTypeDeterminator();
-            double[] sides = mainReader.ReadDoublesFromConsoleUntilSuccess();
-            Triangle mainTriangle = new Triangle(sides);
-            mainDeterminator.DetermineAndShowType(mainTriangle);
-            Console.ReadKey();
+            ok = 0,
+            exceptionInMain = -1,
+            generalException = -2
+        }
+        private const string ERROR_MAIN_MESSAGE = "Looks like something really bad happened, halting with error code";
+        static int Main(string[] args)
+        {
+            try
+            {
+                ConsoleReader mainReader = new ConsoleReader();
+                TriangleTypeDeterminator mainDeterminator = new TriangleTypeDeterminator();
+                double[] sides = mainReader.ReadDoublesFromConsoleUntilSuccess();
+                Triangle mainTriangle = new Triangle(sides);
+                mainDeterminator.DetermineAndShowType(mainTriangle);
+                Console.ReadKey();
+                return (int)MainStatusCodes.ok;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine(ERROR_MAIN_MESSAGE);
+                return (int)MainStatusCodes.exceptionInMain;
+            }
         }
     }
 }
