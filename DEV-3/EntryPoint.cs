08eacc5338af31095d4a45fc3941141dcf79e8bc
IgorBalykin <nuclearcrafter@mail.ru>
2017-07-08 13:18:42 +0300

    Added exception handling in main

--- DEV-3/EntryPoint.cs
+++ DEV-3/EntryPoint.cs
@@ -1,11 +1,29 @@
-﻿namespace DEV_3
+﻿using System;
+
+namespace DEV_3
 {
     class EntryPoint
     {
-        static void Main(string[] args)
+        enum MainStatusCodes
         {
-            FibNumConsoleReader mainReader = new FibNumConsoleReader();
-            mainReader.ReadNumber();
+            ok = 0,
+            exceptionInMain = -1,
+            generalException = -2
+        }
+        private const string ERROR_MAIN_MESSAGE = "Looks like something really bad happened, halting with error code";
+        static int Main(string[] args)
+        {
+            try
+            {
+                FibNumConsoleReader mainReader = new FibNumConsoleReader();
+                mainReader.ReadNumber();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine(ERROR_MAIN_MESSAGE);
+                return (int)MainStatusCodes.exceptionInMain;
+            }
+            return (int)MainStatusCodes.ok;
         }
     }
 }
